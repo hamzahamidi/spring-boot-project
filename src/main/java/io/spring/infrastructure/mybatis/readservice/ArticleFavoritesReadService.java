42001672e4bc5b8f3e24efa91231e601688fceb3
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-06-03 20:25:27 +0200

    Initial commit
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

---
+++ src/main/java/io/spring/infrastructure/mybatis/readservice/ArticleFavoritesReadService.java
@@ -0,0 +1,22 @@
+package io.spring.infrastructure.mybatis.readservice;
+
+import io.spring.application.data.ArticleFavoriteCount;
+import io.spring.core.user.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+
+@Mapper
+@Component
+public interface ArticleFavoritesReadService {
+    boolean isUserFavorite(@Param("userId") String userId, @Param("articleId") String articleId);
+
+    int articleFavoriteCount(@Param("articleId") String articleId);
+
+    List<ArticleFavoriteCount> articlesFavoriteCount(@Param("ids") List<String> ids);
+
+    Set<String> userFavorites(@Param("ids") List<String> ids, @Param("currentUser") User currentUser);
+}
